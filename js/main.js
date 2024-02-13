0bb2c97e7f717dfb5fef24eeb527a6aee6d706b3
Сергей Субботин <zarodiny@yandex.ru>
2024-02-13 10:19:18 +1000

    feat: добавляет стили

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { initSlider } from "./module/sliders.js";
 import { initCurrentYear } from "./module/current-year.js";
 import { initScrollToTop } from "./module/scroll-to-top.js";
 import { initScroll } from "./module/scroll.js";
+import { initPopup } from "./module/popup.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('подключен скрипт main.js');
@@ -12,7 +13,8 @@ window.addEventListener('DOMContentLoaded', () => {
     initCurrentYear();
     initScrollToTop();
     initScroll();
-    baguetteBox.run('.gallery-wrapper');
+    // baguetteBox.run('.gallery-wrapper');
+    initPopup();
 
     // DOMContentLoaded
 });
